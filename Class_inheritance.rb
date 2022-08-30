e4892d2e0b4eda3c36ab334513936c4a127f548f
Brendan <brendantsuda@gmail.com>
2022-08-30 13:53:46 -0700

    Attempted manager bonus method

--- Class_inheritance.rb
+++ Class_inheritance.rb
@@ -6,7 +6,6 @@ class Employee
         @name = name
         @title = title
         @salary = salary
-        if 
         @boss = boss
         
     end
@@ -19,13 +18,20 @@ end
 
 class Manager < Employee
 
-    def initialize
-        @employee = []
+    def initialize(name, title, salary, boss, employees)
+        super(name, title, salary, boss)
+        @employees = employees
     end
     
     def bonus(multiplier)
-
+        bonus = 0
+        @employees.each do |employee|
+            if employee.is_a?(Manager)
+                bonus += (employee.salary * multiplier) + employee.bonus(multiplier)
+            else
+                bonus += (employee.salary * multiplier)
+            end
+        end
+        bonus
     end
-
-
 end
