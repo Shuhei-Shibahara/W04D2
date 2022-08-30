a0b8a30644dc8a309df0f1e0fc701ef758fe6357
Shuhei Shibahara <Shoehey06@gmail.com>
2022-08-30 14:17:46 -0700

    Finished class inheritance

--- Class_inheritance.rb
+++ Class_inheritance.rb
@@ -35,3 +35,10 @@ class Manager < Employee
         bonus
     end
 end
+david = Employee.new('David', 'TA', 10000, 'darren')
+shawna = Employee.new('Shawna', 'TA', 12000, 'darren')
+darren = Manager.new('Darren','TA Manager', 78000, 'ned', [david, shawna])
+ned = Manager.new('Ned', 'Founder',1000000, nil, [darren])
+p ned.bonus(5) # => 500_000
+p darren.bonus(4) # => 88_000
+p david.bonus(3) # => 30_000
