e5a702da05bb06b3b947a54c0a4e5e0ba4bdf99a
Shuhei Shibahara <Shoehey06@gmail.com>
2022-08-31 11:24:07 -0700

    did grow unblocked

--- Chess/Pieces/slideable.rb
+++ Chess/Pieces/slideable.rb
@@ -1,8 +1,27 @@
 module Slideable
 
-    def horizontal_dirs
-        
+    def moves
+
+    end
     
+    def grow_unblocked_moves_in_dir(dx,dy)
+        position_in_dir = []
+        new_pos = add_position(self.pos, [dx,dy] )
+        until !self.board.valid_pos?(new_pos) 
+            if board[new_pos] != nil
+                if self.color != board[new_pos].color
+                    position_in_dir << new_pos 
+                    return position_in_dir
+                else
+                    return position_in_dir
+                end
+            end
+            position_in_dir << new_pos 
+            new_pos = add_position(new_pos, [dx,dy] )
+        end
+        position_in_dir
+    end
+        
     def add_position(pos1, pos2)
         [pos1.first + pos2.first, pos1.last + pos2.last]
     end
